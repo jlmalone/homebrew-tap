2592a74e4a67e42ed54f0744fa2a6c1b6f4ae5ac
Joseph Malone <jlmalone@users.noreply.github.com>
2026-05-04 03:46:44 -0700

    Point mythoman cask to homebrew-tap releases (repo is private)

--- Casks/mythoman.rb
+++ Casks/mythoman.rb
@@ -2,7 +2,7 @@ cask "mythoman" do
   version "1.0.0"
   sha256 "5ae33423fc4ac7b792a2b3b99237c5cd766a1667331c1d0e6fdc61aa8f555841"
 
-  url "https://github.com/jlmalone/mythoman/releases/download/v#{version}/Mythoman-#{version}.dmg"
+  url "https://github.com/jlmalone/homebrew-tap/releases/download/v#{version}/Mythoman-#{version}.dmg"
   name "Mythoman"
   desc "Per-install utility shell and project launcher"
   homepage "https://github.com/jlmalone/mythoman"
