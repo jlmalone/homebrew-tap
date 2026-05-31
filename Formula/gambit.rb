320db7b539667d0db0c21a818270bde837b46ec7
Joseph Malone <jlmalone@users.noreply.github.com>
2026-05-30 21:17:44 -0700

    Bump gambit to 0.4.0 (play the AI)

--- Formula/gambit.rb
+++ Formula/gambit.rb
@@ -2,7 +2,7 @@ class Gambit < Formula
   desc "Pure-Kotlin CLI client for the Gambit chess DAG + tracker protocol"
   homepage "https://github.com/jlmalone/gambit_cli"
   url "https://github.com/jlmalone/homebrew-tap/releases/download/gambit-v0.4.0/gambit-0.4.0.zip"
-  sha256 "ea9a416dbffa7b59eaa568622045551bafbbd66a768348b28d9d65c40880b639"
+  sha256 "6472968a14af2bcb13e0488456e672f7e324663bf0a3e7f3cc746c2efe2d81ee"
   version "0.4.0"
 
   depends_on "openjdk@21"
