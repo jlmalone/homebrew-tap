8ee82bd01e277c6f2dd7f6f87673e74c86f5958d
Joseph Malone <jlmalone@users.noreply.github.com>
2026-05-30 18:20:54 -0700

    Bump gambit to 0.2.1 (matchmaking + home menu)

--- Formula/gambit.rb
+++ Formula/gambit.rb
@@ -2,7 +2,7 @@ class Gambit < Formula
   desc "Pure-Kotlin CLI client for the Gambit chess DAG + tracker protocol"
   homepage "https://github.com/jlmalone/gambit_cli"
   url "https://github.com/jlmalone/homebrew-tap/releases/download/gambit-v0.2.1/gambit-0.2.1.zip"
-  sha256 "10b524baeb0c271992958fe6adbd9562068a1a48313e9c783b3c69a82985c09e"
+  sha256 "ccf8fcb4b6de2a3e17b9d5a0e349cd8c3d292c7201e7947723030d9fccfecd92"
   version "0.2.1"
 
   depends_on "openjdk@21"
