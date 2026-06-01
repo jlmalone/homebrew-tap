7b685b1ad7a9d0952a5b44c8667a896200e1dd4e
Joseph Malone <jlmalone@users.noreply.github.com>
2026-06-01 01:38:38 -0700

    Bump gambit to 0.7.0 (clocks + LLM harness + REST API)

--- Formula/gambit.rb
+++ Formula/gambit.rb
@@ -1,9 +1,9 @@
 class Gambit < Formula
   desc "Pure-Kotlin CLI client for the Gambit chess DAG + tracker protocol"
   homepage "https://github.com/jlmalone/gambit_cli"
-  url "https://github.com/jlmalone/homebrew-tap/releases/download/gambit-v0.4.0/gambit-0.4.0.zip"
-  sha256 "6472968a14af2bcb13e0488456e672f7e324663bf0a3e7f3cc746c2efe2d81ee"
-  version "0.4.0"
+  url "https://github.com/jlmalone/homebrew-tap/releases/download/gambit-v0.7.0/gambit-0.7.0.zip"
+  sha256 "2679ecc3477ee93524ba07779364b331ce4ec7fa92850d8f7323904ea304776c"
+  version "0.7.0"
 
   depends_on "openjdk@21"
 
@@ -15,6 +15,6 @@ class Gambit < Formula
   end
 
   test do
-    assert_match "0.4.0", shell_output("#{bin}/gambit version")
+    assert_match "0.7.0", shell_output("#{bin}/gambit version")
   end
 end
