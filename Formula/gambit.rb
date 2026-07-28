83eda0765f0347b8daca4bd4e40be242d419634e
Joseph Malone <jlmalone@users.noreply.github.com>
2026-07-28 02:41:08 -0700

    Release Gambit CLI 0.59.6

--- Formula/gambit.rb
+++ Formula/gambit.rb
@@ -1,9 +1,9 @@
 class Gambit < Formula
   desc "Pure-Kotlin CLI client for the Gambit chess DAG + tracker protocol"
   homepage "https://github.com/jlmalone/gambit_cli"
-  url "https://github.com/jlmalone/homebrew-tap/releases/download/gambit-v0.59.5/gambit-0.59.5.zip"
-  version "0.59.5"
-  sha256 "5dd0a466817e27fdbf1940ed3938ad48ef7d1b04e54aaafd75411412ff2ceb3f"
+  url "https://github.com/jlmalone/homebrew-tap/releases/download/gambit-v0.59.6/gambit-0.59.6.zip"
+  version "0.59.6"
+  sha256 "709bd59d388e07f1b42e64ae7aa9443b14286490aead3a149ce23a46199384b9"
 
   depends_on "openjdk@21"
 
@@ -15,6 +15,6 @@ class Gambit < Formula
   end
 
   test do
-    assert_match "0.59.5", shell_output("#{bin}/gambit version")
+    assert_match "0.59.6", shell_output("#{bin}/gambit version")
   end
 end
